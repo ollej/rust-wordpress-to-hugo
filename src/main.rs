385b7ab33d656167af1859775b394dacb5830cf7
Olle Wreede <olle@wreede.se>
2023-07-03 15:14:28 +0200

    Add terms to frontmatter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use html2md::parse_html;
 use regex::Regex;
 use sqlx::mysql::MySqlPoolOptions;
-use sqlx::types::chrono::{NaiveDate, NaiveDateTime};
+use sqlx::types::chrono::NaiveDateTime;
 use std::io::prelude::*;
 use std::{fs, io};
 use unidecode::unidecode;
 
+static DATABASE_URL: &str = "mysql://root:password@127.0.0.1:3306/wordpressdb";
 static DIR: &str = "./content";
 
 #[derive(Debug, sqlx::FromRow)]
@@ -35,6 +36,7 @@ struct Post {
     parent: u64,
     #[sqlx(rename = "post_status")]
     status: String,
+    terms: String,
 }
 
 #[derive(Debug)]
@@ -45,6 +47,7 @@ struct HugoContent {
     date: NaiveDateTime,
     content: String,
     draft: bool,
+    tags: Vec<String>,
 }
 
 impl HugoContent {
@@ -69,6 +72,7 @@ Title = "{}"
 Date = "{}"
 Author = "{}"
 Draft = {}
+Tags = {:?}
 +++
 
 {}"#,
@@ -76,6 +80,7 @@ Draft = {}
             self.date,
             self.author,
             self.draft,
+            self.tags,
             self.content.replace("\r", ""),
         )
     }
@@ -91,15 +96,20 @@ fn sanitize(filename: String) -> String {
 async fn main() -> Result<(), sqlx::Error> {
     let pool = MySqlPoolOptions::new()
         .max_connections(5)
-        .connect("mysql://root:password@127.0.0.1:3306/wordpressdb")
+        .connect(DATABASE_URL)
         .await?;
 
     // Read wordpress data
     // TODO: Read pages
     // TODO: Handle attachments
-    // TODO: Handle terms
     let posts = sqlx::query_as::<_, Post>(
-        "SELECT p.ID, user_nicename, post_date, post_content, post_title, post_name, post_type, post_mime_type, post_parent, post_status FROM wp_posts AS p, wp_users AS u WHERE p.post_author = u.ID AND post_type = 'post' AND post_status = 'publish'",
+        "SELECT p.ID, user_nicename, post_date, post_content, post_title, post_name, post_type, post_mime_type, post_parent, post_status, GROUP_CONCAT(t.name) AS terms
+        FROM wp_posts AS p
+        INNER JOIN wp_users AS u ON p.post_author = u.ID 
+        INNER JOIN wp_term_relationships AS tr ON p.ID = tr.object_id
+        INNER JOIN wp_terms AS t ON tr.term_taxonomy_id = t.term_id
+        WHERE post_type = 'post' AND post_status = 'publish'
+        GROUP BY p.ID",
     )
     .fetch_all(&pool)
     .await?;
@@ -107,13 +117,19 @@ async fn main() -> Result<(), sqlx::Error> {
     // Create Hugo content
     fs::create_dir_all(&DIR)?;
     for post in posts {
+        let author = if post.author == "admin".to_string() || post.author == "olle".to_string() {
+            "Olle Wreede".to_string()
+        } else {
+            post.author
+        };
         let content = HugoContent {
             category: "blog".to_string(),
             title: post.title,
-            author: post.author,
+            author,
             date: post.date,
             content: parse_html(&post.content),
             draft: post.status == "publish",
+            tags: post.terms.split(",").map(String::from).collect(),
         };
         content.write()?;
     }
